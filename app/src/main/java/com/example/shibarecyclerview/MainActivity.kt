563ff7ff276cbe4f6dd5fa0b84875c70c8a493c6
Barret Vogtman <phoenixredwolf@verizon.net>
2021-07-09 12:19:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/shibarecyclerview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.shibarecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shibarecyclerview.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
